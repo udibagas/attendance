3e1ad6d4dbb8fd7c2bc5a1d97e5f77e5755a0628
Bagas Udi Sahsangka <udibagas@gmail.com>
2024-04-21 20:37:12 +0700

    employees

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -2,10 +2,25 @@ const { Employee } = require("../models");
 
 module.exports = {
   async index(req, res, next) {
-    const { page, search, pageSize = 15 } = req.query;
+    const {
+      page = 1,
+      search,
+      pageSize = 15,
+      sort_column: column = "name",
+      sort_order: order = "asc",
+    } = req.query;
     try {
-      const emplooyees = await Employee.paginate(page, pageSize, search);
-      res.json(emplooyees);
+      const data = await Employee.paginate(page, pageSize, search, {
+        column,
+        order,
+      });
+
+      res.render("layout", {
+        view: "employees",
+        data,
+        query: { ...req.query, page },
+        route: "/employees",
+      });
     } catch (error) {
       next(error);
     }
